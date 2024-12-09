738e82eab8ec3ab4851479f81783f5422f736d45
scabrerof <scabrero@dru-id.com>
2024-12-09 10:42:56 +0100

    Add logic to force logout when Identity is not logged in DruID but logged in WP

--- lib/WP_Druid/Admin/Services/Admin_Menu_Manager.php
+++ lib/WP_Druid/Admin/Services/Admin_Menu_Manager.php
@@ -2,8 +2,6 @@
 
 use WP_Druid\Admin\Controllers\Error_Log;
 use WP_Druid\Admin\Controllers\Home;
-use WP_Druid\Admin\Controllers\Promotions;
-use WP_Druid\Admin\Controllers\Rewrites;
 
 /**
  * @package WP Druid
@@ -28,11 +26,7 @@ class Admin_Menu_Manager
             // Error logs.
             add_submenu_page($main_menu, __('Error Log', WPDR_LANG_NS), __('Error Log', WPDR_LANG_NS), 'manage_options', 'druid-errors', function(){ druid_x(new Error_Log())->index(); }, 80.5, 0 );
 
-            // Refresh Rewrties.
-            add_submenu_page($main_menu, __('Rewrites', WPDR_LANG_NS), __('Rewrites', WPDR_LANG_NS), 'manage_options', 'druid-rewrites', function(){ druid_x(new Rewrites())->index(); }, 80.5, 0 );
-
-            //add_submenu_page($main_menu, __('Promotions', WPDR_LANG_NS), __('Promotions', WPDR_LANG_NS), 'manage_options', 'druid-promotions', function(){ druid_x(new Promotions())->index(); }, 80.5, 0 );
-        });
+            });
 
         do_action('acf/input/admin_head');
         do_action('acf/input/admin_enqueue_scripts');
