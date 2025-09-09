0ad359a8adfb90de964e3462cb55705e78377350
scabrerof <scabrero@dru-id.com>
2025-09-09 15:31:40 +0200

    Process state

--- lib/WP_Druid/Services/Callbacks/Logout.php
+++ lib/WP_Druid/Services/Callbacks/Logout.php
@@ -3,6 +3,7 @@
 use Genetsis\Identity;
 use WP_Druid\Factory\IdentityFactory;
 use WP_Druid\Front\Collections\Callbacks\Post_Login_Parameters;
+use WP_Druid\Services\Errors as Errors_Service;
 use WP_Druid\Services\Users as Users_Service;
 use WP_Druid\Utils\Session\Services\SessionManager;
 use WP_Druid\Contracts\Callbacks\Callbackable as CallbackContract;
@@ -29,5 +30,36 @@ class Logout extends Callback_Base_Service implements CallbackContract
 
         wp_safe_redirect($pageToRedirect);
         exit();
+    }
+
+    private function processState($state)
+    {
+        // Initialize pageToRedirect with default value
+        $pageToRedirect = SessionManager::get_and_forget(WPDR_PREVIOUS_URL_SESSION_KEY, home_url());
+
+        // Decoding the 'state' parameter if it exists
+        if ($state) {
+            try {
+                $json_data = base64_decode($state);
+                $data = json_decode($json_data, true);
+
+                if (is_array($data)) {
+                    $pageToRedirect = $data['pageToRedirect'] ?? $pageToRedirect;
+                    // Encode 'state' attribute and append to the URL if it exists
+                    if (isset($data['state'])) {
+                        $state_attr_base64 = base64_encode(json_encode(['state' => $data['state']]));
+                        $pageToRedirect = add_query_arg('state', $state_attr_base64, $pageToRedirect);
+                    }
+                } else {
+                    // Log error and keep the default URL
+                    Errors_Service::log_error(__CLASS__ . ' (' . __LINE__ . ')', 'Invalid JSON data in state: ' . $json_data);
+                }
+            } catch (\Exception $e) {
+                // Log error and keep the default URL
+                Errors_Service::log_error(__CLASS__ . ' (' . __LINE__ . ')', 'State decoding error: ' . $e->getMessage());
+            }
+        }
+
+        return $pageToRedirect;
     }
 }
